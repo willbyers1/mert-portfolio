9b7c4b44db9cebc790cf399f7fb78b94a77cdfa2
Mert Batu Bülbül <mmerttbatu@gmail.com>
2026-09-19 20:20:31 +0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/mert-portfolio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
